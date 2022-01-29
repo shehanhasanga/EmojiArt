24d14e60f50e5d9429dfa40019717d74b9003145
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-01-29 10:24:25 +0530

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by shehan karunarathna on 2022-01-29.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
